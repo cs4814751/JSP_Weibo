2681f16637e4fd9b4b5f317f8744c2e6cb65fb82
LeiBro <cs4814751@gmail.com>
2015-12-10 17:59:10 +0800

    index dispatcher
    实现输入网址,进行判断,如果登录了跳转到首页,没登录跳转到登录页面

--- src/index.java
+++ src/index.java
@@ -1,35 +1,34 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 public class index extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the GET method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
+		HttpSession session = request.getSession(false);
+		try {
+			String username = (String)session.getAttribute("username");
+			request.setAttribute("username", username);
+			RequestDispatcher view = request.getRequestDispatcher("main.jsp");
+			view.forward(request, response);
+		} catch(NullPointerException e) {
+			response.sendRedirect("login.html");
+		}
 	}
 
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		this.doGet(request, response);
 	}
 
 
